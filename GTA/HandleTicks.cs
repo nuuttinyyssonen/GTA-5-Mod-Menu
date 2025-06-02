ff32a2c5a3f4b46dd30a7ff0014485c95e292661
nuuttinyyssonen <nuutti.nyyssonen@gmail.com>
2025-06-02 10:23:52 +0300

    Ticks are structured in its own file now

---
+++ GTA/HandleTicks.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System;
+using GTA;
+using GTA.Native;
+using GTA.Math;
+using System.Windows.Forms;
+using GTA.UI;
+using GTAV_Mod_Menu;
+
+namespace ticks
+{
+    public class HandleTicks
+    {
+        public static void handleOnTick(object sender, EventArgs e)
+        {
+            if (Main.menuOpen && Main.currentSubMenu == Main.SubMenu.None)
+            {
+                string displayText = "";
+                for (int i = 0; i < Main.menuOptions.Length; i++)
+                {
+                    if (i == Main.selectedMenuIndex)
+                    {
+                        displayText += $"→ {Main.menuOptions[i]}\n";
+                    }
+                    else
+                    {
+                        displayText += $"   {Main.menuOptions[i]}\n";
+                    }
+                }
+                GTA.UI.Screen.ShowSubtitle(displayText, 1);
+            }
+            else if (Main.currentSubMenu == Main.SubMenu.Weather)
+            {
+                string displayText = "Weather Menu:\n";
+                for (int i = 0; i < Main.weatherOptions.Length; i++)
+                {
+                    if (i == Main.selectedSubMenuIndex)
+                    {
+                        displayText += $"→ {Main.weatherOptions[i]}\n";
+                    }
+                    else
+                    {
+                        displayText += $"   {Main.weatherOptions[i]}\n";
+                    }
+                }
+                GTA.UI.Screen.ShowSubtitle(displayText, 1);
+            }
+            else if (Main.currentSubMenu == Main.SubMenu.Vehicle)
+            {
+                string displayText = "Vehicle Menu:\n";
+                for (int i = 0; i < Main.vehicleOptions.Length; i++)
+                {
+                    if (i == Main.selectedSubMenuIndex)
+                    {
+                        displayText += $"→ {Main.vehicleOptions[i]}\n";
+                    }
+                    else
+                    {
+                        displayText += $"   {Main.vehicleOptions[i]}\n";
+                    }
+                }
+                GTA.UI.Screen.ShowSubtitle(displayText, 1);
+            }
+            else if (Main.currentSubMenu == Main.SubMenu.Player)
+            {
+                string displayText = "Player Menu:\n";
+                for (int i = 0; i < Main.playerOptions.Length; i++)
+                {
+                    if (i == Main.selectedSubMenuIndex)
+                    {
+                        displayText += $"→ {Main.playerOptions[i]}\n";
+                    }
+                    else
+                    {
+                        displayText += $"   {Main.playerOptions[i]}\n";
+                    }
+                }
+                GTA.UI.Screen.ShowSubtitle(displayText, 1);
+            }
+            else if (Main.currentSubMenu == Main.SubMenu.World)
+            {
+                string displayText = "World Menu:\n";
+                for (int i = 0; i < Main.worldOptions.Length; i++)
+                {
+                    if (i == Main.selectedSubMenuIndex)
+                    {
+                        displayText += $"→ {Main.worldOptions[i]}\n";
+                    }
+                    else
+                    {
+                        displayText += $"   {Main.worldOptions[i]}\n";
+                    }
+                }
+                GTA.UI.Screen.ShowSubtitle(displayText, 1);
+            }
+        }
+    }
+}
---
+++ GTA/Main.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using GTA;
+using GTA.Native;
+using GTA.Math;
+using System.Windows.Forms;
+using GTA.UI;
+using MenuActions;
+using ExecuteAllMods;
+using Key;
+using ticks;
+
+namespace GTAV_Mod_Menu
+{
+    public class Main : Script
+    {
+        // Define the sub-menu states
+        public enum SubMenu
+        {
+            None,
+            Weather,
+            Vehicle,
+            Player,
+            World
+        }
+        public static SubMenu currentSubMenu = SubMenu.None;
+        public static bool menuOpen = false;
+
+        // Define the selected index for the main menu and sub-menus
+        public static int selectedMenuIndex = 0;
+        public static int selectedSubMenuIndex = 0;
+        public static string[] selectedSubMenuOption;
+
+        // Define the menu options and sub-menu options
+        public static readonly string[] menuOptions = { 
+            "Weather Menu", 
+            "Vehicle Menu", 
+            "Player Menu", 
+            "World Menu"
+        };
+        public static readonly string[] weatherOptions = { 
+            "Cloudy", 
+            "Sunny", 
+            "Rainy", 
+            "Snowy" 
+        };
+        public static readonly string[] vehicleOptions = { 
+            "Adder", 
+            "Zentorno", 
+            "Bullet", 
+            "Cheetah", 
+            "Infernus", 
+            "T20" 
+        };
+        public static readonly string[] playerOptions = { 
+            "God Mode", 
+            "Give Weapons", 
+            "Wanted Level Off", 
+            "Super Speed", 
+            "Infinite Ammo" 
+        };
+        public static readonly string[] worldOptions = { 
+            "Set time of day: Morning", 
+            "Set time of day: Noon", 
+            "Set time of day: night", 
+            "Remove vehicles", 
+            "Low gravity mode", 
+            "Remove pedestrians" 
+        };
+
+        public Main()
+        {
+            Tick += HandleTicks.handleOnTick;
+            KeyDown += HandleKey.handleKeyDown;
+            Interval = 0;
+        }
+    }
+}
